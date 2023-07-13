945ecfd9f1eac362fd0c1cd145700e5f7e942a38
MimHe <mimhe1381@gmail.com>
2023-07-13 22:04:23 +0330

    AJAX Call

---
+++ Asynchronous/AJAX_Call.js
@@ -0,0 +1,107 @@
+
+// Our First AJAX Call: XMLHttpRequest
+
+const countriesContainer = document.querySelector('.countries');
+
+const getCountryData = function (country) {
+    const request = new XMLHttpRequest();
+    // 'GET' => type of request
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send(); // send request
+    // it is being done in background. while the rest of the code keeps running. => Asynchronous Behavior
+    console.log(request.responseText); // nothing
+
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+
+        const html = `<article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+        ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+      <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+    </div>
+  </article>`;
+
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+    });
+};
+
+// getCountryData('iran');
+// getCountryData('usa');
+// getCountryData('germany');
+
+
+// Callback Hell => when we have a lot of nested callbacks => to execute asynchronous tasks in sequence.
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+  ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+      <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+    console.log(neighbour)
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('portugal');
+// getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
